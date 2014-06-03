19527e50e24b4d8f4b1a8d38fbc167400f8998b7
Dominic Monaco <dmonaco05@hotmail.com>
2014-06-03 12:51:07 -0400

    finished app

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# To add service: add column to shows table & add it to constant in shows model
+# Change prices of services in shows model
+
+# Network.create(name:"ABC", image:"abc.png")
+# Show.create(name:"Shark Tank", network_id:##, hulu:true, netflix:false, itunes:0, amazon:2.59)
+
+# User.create(email:"",password:"")
